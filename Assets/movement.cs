10a275cb1dfb6f4dd4d465610906acbd248c8616
Daniel <Danevans0223@gmail.com>
2020-05-18 11:25:21 -0400

    Initial commit

---
+++ Assets/movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 5f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float mouseInput = Input.GetAxis("Mouse X");
+        //Vector3 lookhere = new Vector3(0, mouseInput, 0);
+        //transform.Rotate(lookhere);
+        float mouseX = (Input.mousePosition.x / Screen.width) - 0.5f;
+        float mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
+        transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime, 0f);
+    }
+}
